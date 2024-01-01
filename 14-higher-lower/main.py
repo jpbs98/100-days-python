dc1e488e064a49bbf9fd78945c5bf3bb89d5ec1d
João Silva <jpsjoaosilva98@gmail.com>
2024-01-01 21:44:03 +0000

    fix swapping lowest follower count check to object_a

--- 14-higher-lower/main.py
+++ 14-higher-lower/main.py
@@ -21,28 +21,27 @@ def main():
         guess = input("Who has more followers? Type 'A' or 'B': ")
         answer = higher_follower_count(object_a, object_b)
 
-        if answer == 'A' and guess == "A":
-            score += 1
-            object_a = random.choice(data)
-
-        if answer == 'B' and guess == "B":
-            score += 1
-            object_b = random.choice(data)
-
-        if guess != answer:
+        if guess != answer[0]:
             print(f"Sorry, that's wrong. Final score: {score}")
             break
+
+        if (answer[0] == 'A' and guess == "A") or (answer[0] == 'B' and guess == "B"):
+            score += 1
+            object_a = answer[1]
+            object_b = random.choice(data)
+
 
         os.system('cls' if os.name == 'nt' else 'clear')
         print(logo)
         print(f"You're right! Current score: {score}.")
 
 
-def higher_follower_count(obj1, obj2):
-    if obj1["follower_count"] > obj2["follower_count"]:
-        return "A"
+def higher_follower_count(obj_a, obj_b):
+    # return opposing object for swap to object_a
+    if obj_a["follower_count"] > obj_b["follower_count"]:
+        return "A", obj_b
     else:
-        return "B"
+        return "B", obj_a
 
 
 if __name__ == '__main__':

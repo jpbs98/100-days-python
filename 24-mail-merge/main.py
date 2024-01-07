f5b51fef863a1f5dda09fff2989d6f2fb80f874a
jpbs98 <jpsjoaosilva98@gmail.com>
2024-01-07 22:23:22 +0000

    finish day 24

---
+++ 24-mail-merge/main.py
@@ -0,0 +1,21 @@
+with open(
+    "24-mail-merge/Input/Names/invited_names.txt", mode="r", encoding="utf-8"
+) as f:
+    names = f.read().splitlines()
+
+
+with open(
+    "24-mail-merge/Input/Letters/starting_letter.txt", mode="r", encoding="utf-8"
+) as f:
+    letter = f.readlines()
+
+
+for name in names:
+    with open(
+        f"24-mail-merge/Output/ReadyToSend/letter_for_{name}.txt",
+        mode="w",
+        encoding="utf-8",
+    ) as f:
+        f.write(letter[0].replace("[name]", name))
+        for line in letter[1:]:
+            f.write(line)

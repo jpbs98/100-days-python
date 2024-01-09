58b65d390643954419c5209508a3663b88d98e8a
João Silva <jpsjoaosilva98@gmail.com>
2024-01-09 15:47:58 +0000

    support special chars

--- 26-nato-alphabet/main.py
+++ 26-nato-alphabet/main.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("nato_phonetic_alphabet.csv")
 
-code = {row.letter: row.code for index, row in df.iterrows()}
+code = {row.letter: row.code for _, row in df.iterrows()}
 
 input_word = input("Enter a word: ").upper()
 
-output_list = [code[letter] for letter in input_word]
+output_list = [code[letter] if letter in code else letter for letter in input_word]
 print(output_list)

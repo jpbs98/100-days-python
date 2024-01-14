5b80fbbbba446276523fef1f2e5411c42c4dd7f7
João Silva <jpsjoaosilva98@gmail.com>
2024-01-14 18:24:37 +0000

    finish day 31

--- 31-flashcards/main.py
+++ 31-flashcards/main.py
@@ -1,7 +1,41 @@
-from tkinter import Tk, Canvas, PhotoImage, Button, Label
-
+import random
+from tkinter import Tk, Canvas, PhotoImage, Button
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+
+try:
+    data = pd.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv("./data/french_words.csv")
+
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card, image=card_front)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card, image=card_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+
+def known_card():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -9,22 +43,25 @@ window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 
+flip_timer = window.after(3000, flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="./images/card_front.png")
 card_back = PhotoImage(file="./images/card_back.png")
 card = canvas.create_image(400, 263, image=card_front)
 canvas.grid(row=0, column=0, columnspan=2)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, font=("Ariel", 60, "bold"))
 
 
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0, command=known_card)
 right_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
